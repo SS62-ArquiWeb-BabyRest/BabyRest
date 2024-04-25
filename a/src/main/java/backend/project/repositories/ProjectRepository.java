6ec8eef8fb39192cff377eb52c7d242f67915ddd
SandroGonzales <116108197+SandroGonzales@users.noreply.github.com>
2024-04-25 00:24:47 -0500

    Revert "avance1"

--- a/src/main/java/backend/project/repositories/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.project.repositories;
-
-import backend.project.entities.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
--- a/src/main/java/backend/project/services/AuthorityService.java
+++
@@ -1,7 +0,0 @@
-package backend.project.services;
-
-import backend.project.entities.Authority;
-
-public interface AuthorityService {
-    public Authority save(Authority authority);
-}
