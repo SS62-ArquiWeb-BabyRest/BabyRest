7b862922cb2e44e73ed91de5e6c0067a72f3ed09
Carolina <carolina.nicole.0705@gmail.com>
2024-04-26 10:18:48 -0500

    inicializando favoritos

--- src/main/java/backend/entities/Favoritos.java
+++ src/main/java/backend/entities/Favoritos.java
@@ -1,5 +1,25 @@
 package backend.entities;
 
 public class Favoritos {
+    private int idFavorito;
+    private int padreEmpleadorId;
+    private int nineraUniversitarioId;
 
+
+    public Favoritos(int idFavorito, int padreEmpleadorId, int nineraUniversitarioId) {
+        this.idFavorito = idFavorito;
+        this.padreEmpleadorId = padreEmpleadorId;
+        this.nineraUniversitarioId = nineraUniversitarioId;
+    }
+
+    public static void main(String[] args) {
+
+        Favoritos favorito = new Favoritos(1, 456, 123);
+
+
+        System.out.println("ID Favorito: " + favorito.getIdFavorito());
+        System.out.println("ID del padre empleador: " + favorito.getPadreEmpleadorId());
+        System.out.println("ID de la niñera/universitario: " + favorito.getNineraUniversitarioId());
+    }
 }
+
