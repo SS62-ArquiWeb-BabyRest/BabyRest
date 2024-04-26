acf7f422a2062a1bf7ad1686df01740f571496f4
Carolina <carolina.nicole.0705@gmail.com>
2024-04-26 10:13:58 -0500

    inicializando entidad disponibilidad

--- src/main/java/backend/entities/Disponibilidad.java
+++ src/main/java/backend/entities/Disponibilidad.java
@@ -1,4 +1,30 @@
 package backend.entities;
 
 public class Disponibilidad {
+    private int id;
+    private int dia;
+    private int horaIni;
+    private int horaFin;
+    private int nineraUniversitarioId;
+
+    // Constructor
+    public Disponibilidad(int id, int dia, int horaIni, int horaFin, int nineraUniversitarioId) {
+        this.id = id;
+        this.dia = dia;
+        this.horaIni = horaIni;
+        this.horaFin = horaFin;
+        this.nineraUniversitarioId = nineraUniversitarioId;
+    }
+
+    public static void main(String[] args) {
+        // Crear una instancia de Disponibilidad
+        Disponibilidad disponibilidad = new Disponibilidad(1, 2, 8, 17, 123);
+
+        // Acceder a las variables miembro
+        System.out.println("ID: " + disponibilidad.getId());
+        System.out.println("Día: " + disponibilidad.getDia());
+        System.out.println("Hora Inicial: " + disponibilidad.getHoraIni());
+        System.out.println("Hora Final: " + disponibilidad.getHoraFin());
+        System.out.println("ID de la niñera/universitario: " + disponibilidad.getNineraUniversitarioId());
+    }
 }
--- src/main/java/backend/entities/Favoritos.java
+++ src/main/java/backend/entities/Favoritos.java
@@ -1,5 +1,5 @@
 package backend.entities;
 
 public class Favoritos {
-    
+
 }
